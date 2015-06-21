e3f198d166d2f0ec0ec5787b83fdb51c9432275f
mycarlos26 <mycarlos_26@hotmail.com>
2015-06-21 12:20:01 -0430

    alquiler

--- Monografico/SADIsoft/vw/ContratoAlquiler.aspx.designer.cs
+++ Monografico/SADIsoft/vw/ContratoAlquiler.aspx.designer.cs
@@ -58,22 +58,13 @@ namespace SADIsoft.vw {
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
         
         /// <summary>
-        /// Control TextBox1.
+        /// Control txtDia.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
-        /// <summary>
-        /// Control RequiredFieldValidator1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        protected global::System.Web.UI.WebControls.TextBox txtDia;
         
         /// <summary>
         /// Control Button1.
